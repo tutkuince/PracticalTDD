d2503f294a2dcb96b6699f7be51964a403bb1c72
tutkuince <tutku.ince@outlook.com>
2019-02-25 19:09:00 +0300

    added addCourse() method to Student

--- src/main/java/com/muditasoft/part02/Student.java
+++ src/main/java/com/muditasoft/part02/Student.java
@@ -1,12 +1,18 @@
 package com.muditasoft.part02;
 
+import com.muditasoft.part01.model.LecturerCourseRecord;
+import com.muditasoft.part01.model.StudentCourseRecord;
+
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Student {
     private final String id;
     private final String name;
     private final String surname;
     private LocalDate birthDate;
+    private Set<StudentCourseRecord> studentCourseRecords = new HashSet<>();
 
     public Student(String id, String name, String surname) {
         this.id = id;
@@ -39,6 +45,24 @@ public class Student {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public void addCourse(LecturerCourseRecord lecturerCourseRecord) {
+
+        if (lecturerCourseRecord == null) {
+            throw new IllegalArgumentException("Can't add course with null lecturer course record");
+        }
+
+        final StudentCourseRecord studentCourseRecord = new StudentCourseRecord(lecturerCourseRecord);
+        studentCourseRecords.add(studentCourseRecord);
+    }
+
+    public Set<StudentCourseRecord> getStudentCourseRecords() {
+        return studentCourseRecords;
+    }
+
+    public void setStudentCourseRecords(Set<StudentCourseRecord> studentCourseRecords) {
+        this.studentCourseRecords = studentCourseRecords;
     }
 
     @Override
