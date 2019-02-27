5194fb49e2f1446cb751e6d8c1e67c084aa57447
tutkuince <tutku.ince@outlook.com>
2019-02-27 17:18:19 +0300

    Dynamic Test

--- src/test/java/com/muditasoft/part02/StudentTestWithDynamicTest.java
+++ src/test/java/com/muditasoft/part02/StudentTestWithDynamicTest.java
@@ -4,18 +4,29 @@ import com.muditasoft.part01.model.Course;
 import com.muditasoft.part01.model.LecturerCourseRecord;
 import com.muditasoft.part01.model.Semester;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.ThrowingConsumer;
 
+import java.util.Iterator;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.DynamicContainer.dynamicContainer;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+import static org.junit.jupiter.api.DynamicTest.stream;
 
 public class StudentTestWithDynamicTest {
 
+    private Student student;
+
+    @BeforeEach
+    void setUp() {
+        student = new Student("1", "Tutku", "Ince");
+    }
+
     @TestFactory
     Stream<DynamicNode> addCourseToStudentWithCourseCodeAndCourseType() {
-        final Student student = new Student("id1", "Tutku", "Ince");
+
 
         return Stream.of("101", "103", "105")
                 .map(courseCode -> dynamicContainer("Add course<" + courseCode + "> to student",
@@ -27,5 +38,21 @@ public class StudentTestWithDynamicTest {
                                     assertTrue(student.isTakeCourse(course));
                                 })))
                 );
+    }
+
+    @TestFactory
+    Stream<DynamicTest> addCourseToStudentWithCourseCode() throws Exception {
+
+        final Iterator<String> courseCodeGenerator = Stream.of("101", "103", "105").iterator();
+        Function<String, String> displayNameGenerator = courseCode -> "Add course<" + courseCode + "> to student";
+        ThrowingConsumer<String> testExecutor = courseCode -> {
+
+            final Course course = Course.newCourse().withCode(courseCode).withCourseType(Course.CourseType.MANDATORY).course();
+            final LecturerCourseRecord lecturerCourseRecord = new LecturerCourseRecord(course, new Semester());
+            student.addCourse(lecturerCourseRecord);
+            assertTrue(student.isTakeCourse(course));
+        };
+
+        return stream(courseCodeGenerator, displayNameGenerator, testExecutor);
     }
 }
