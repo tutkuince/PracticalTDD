8212be8f6679b2053e39311b400e0313d2516ed1
tutkuince <tutku.ince@outlook.com>
2019-03-04 16:06:27 +0300

    new methods

--- src/main/java/com/muditasoft/part03/Student.java
+++ src/main/java/com/muditasoft/part03/Student.java
@@ -1,10 +1,11 @@
 package com.muditasoft.part03;
 
-import com.muditasoft.part01.model.Course;
-import com.muditasoft.part01.model.LecturerCourseRecord;
-import com.muditasoft.part01.model.StudentCourseRecord;
+import com.muditasoft.part01.model.*;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,6 +15,7 @@ public class Student {
     private final String surname;
     private LocalDate birthDate;
     private Set<StudentCourseRecord> studentCourseRecords = new HashSet<>();
+    private Department department;
 
     public Student(String id, String name, String surname) {
         this.id = id;
@@ -26,6 +28,41 @@ public class Student {
         this.name = name;
         this.surname = surname;
         this.birthDate = birthDate;
+    }
+
+    public Student(String id, String name, String surname, Set<StudentCourseRecord> studentCourseRecords) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.studentCourseRecords.addAll(studentCourseRecords);
+    }
+
+    public void addCourse(LecturerCourseRecord lecturerCourseRecord) {
+
+        if (lecturerCourseRecord == null) {
+            throw new IllegalArgumentException("Can't add course with null lecturer course record");
+        }
+
+        final StudentCourseRecord studentCourseRecord = new StudentCourseRecord(lecturerCourseRecord);
+        studentCourseRecords.add(studentCourseRecord);
+    }
+
+    public void dropCourse(LecturerCourseRecord lecturerCourseRecord) {
+
+        if (lecturerCourseRecord == null) {
+            throw new IllegalArgumentException("Can't drop course with null lecturer course record");
+        }
+
+        final StudentCourseRecord studentCourseRecordWillBeDropped = studentCourseRecords.stream()
+                .filter(studentCourseRecord -> studentCourseRecord.getLecturerCourseRecord().equals(lecturerCourseRecord))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("There is no student course record for given lecturer course record"));
+
+        if (!studentCourseRecordWillBeDropped.getLecturerCourseRecord().getSemester().isAddDropAllowed()) {
+            throw new NotActiveSemesterException("Add drop period is closed for the semester");
+        }
+
+        studentCourseRecords.remove(studentCourseRecordWillBeDropped);
     }
 
     public String getId() {
@@ -48,37 +85,63 @@ public class Student {
         this.birthDate = birthDate;
     }
 
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 
     public Set<StudentCourseRecord> getStudentCourseRecords() {
-        return studentCourseRecords;
-    }
-
-    public void setStudentCourseRecords(Set<StudentCourseRecord> studentCourseRecords) {
-        this.studentCourseRecords = studentCourseRecords;
-    }
-
-    public void addCourse(LecturerCourseRecord lecturerCourseRecord) {
-
-        if (lecturerCourseRecord == null) {
-            throw new IllegalArgumentException("Can't add course with null lecturer course record");
-        }
-
-        final StudentCourseRecord studentCourseRecord = new StudentCourseRecord(lecturerCourseRecord);
-        studentCourseRecords.add(studentCourseRecord);
+        return Collections.unmodifiableSet(studentCourseRecords);
     }
 
     public boolean isTakeCourse(Course course) {
-        return studentCourseRecords.stream().map(StudentCourseRecord::getLecturerCourseRecord)
-                .map(LecturerCourseRecord::getCourse).anyMatch(course1 -> course1.equals(course));
+        return studentCourseRecords.stream()
+                .map(StudentCourseRecord::getLecturerCourseRecord)
+                .map(LecturerCourseRecord::getCourse)
+                .anyMatch(course1 -> course1.equals(course));
+    }
+
+    public void addGrade(LecturerCourseRecord lecturerCourseRecord, StudentCourseRecord.Grade grade) {
+
+        final StudentCourseRecord studentCourseRecord1 = studentCourseRecords.stream()
+                .filter(studentCourseRecord -> studentCourseRecord.getLecturerCourseRecord().equals(lecturerCourseRecord))
+                .findAny()
+                .orElseThrow(() ->
+                        new IllegalArgumentException(
+                                new NoCourseFoundForStudentException(String.format("Student didn't take any course for lecturer course record: %s", lecturerCourseRecord == null ? "null" : lecturerCourseRecord.getCourse().getCode()))
+
+                        )
+
+                );
+        studentCourseRecord1.setGrade(grade);
+    }
+
+    public BigDecimal gpa() {
+
+        int totalCredit = 0;
+        BigDecimal weightedGpa = BigDecimal.ZERO;
+
+        for (StudentCourseRecord studentCourseRecord : studentCourseRecords) {
+            totalCredit += courseCredit(studentCourseRecord);
+            weightedGpa = weightedGpa.add(BigDecimal.valueOf(courseCredit(studentCourseRecord)).multiply(courseGrade(studentCourseRecord)));
+        }
+
+        return weightedGpa.divide(BigDecimal.valueOf(totalCredit), 2, RoundingMode.HALF_UP);
+    }
+
+    private int courseCredit(StudentCourseRecord studentCourseRecord) {
+        return studentCourseRecord.getLecturerCourseRecord().getCredit();
+    }
+
+    private BigDecimal courseGrade(StudentCourseRecord studentCourseRecord) {
+        return studentCourseRecord.getGrade().getGradeInNumber();
     }
 
     @Override
     public String toString() {
-        return "Student{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", birthDate=" + birthDate +
-                '}';
+        return String.format("Id:%s => %s %s", getId(), getName(), getSurname());
     }
 }
