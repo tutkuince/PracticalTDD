7ee09ee023ddf49c547c88fc08644321ae88a556
tutkuince <tutku.ince@outlook.com>
2019-03-04 16:52:52 +0300

    TestExecutionExceptionHandler usage

---
+++ src/test/java/com/muditasoft/part03/IllegalArgumentExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.muditasoft.part03;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
+
+import java.util.logging.Logger;
+
+public class IllegalArgumentExceptionHandler implements TestExecutionExceptionHandler {
+
+    private static Logger logger = Logger.getLogger(IllegalArgumentExceptionHandler.class.getName());
+
+    @Override
+    public void handleTestExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
+        logger.severe(String.format("IllegalArgumentException was thrown by a method %s with description: %s", context.getRequiredTestMethod().getName(), throwable.getMessage()));
+        throw  throwable;
+    }
+}
