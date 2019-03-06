2f5d3f65b002cd52e2f0db697f07a6304365dd75
tutkuince <tutku.ince@outlook.com>
2019-03-06 15:46:39 +0300

    checkISBN

--- src/main/java/com/muditasoft/part04/ValidateISBN.java
+++ src/main/java/com/muditasoft/part04/ValidateISBN.java
@@ -3,9 +3,12 @@ package com.muditasoft.part04;
 public class ValidateISBN {
 
     public boolean checkISBN(String isbn) {
-        if (isbn.equals("0140449116")) {
-            return true;
+        int total = 0;
+
+        for (int i = 0; i < 10; i++){
+            total += isbn.charAt(i) * (10 - i);
         }
-        return false;
+
+        return total % 11 == 0;
     }
 }
