bd98176381a7d3d2b40d2db53f26914820569431
tutkuince <tutku.ince@outlook.com>
2019-03-06 16:44:22 +0300

    if check for 9th char is X

--- src/main/java/com/muditasoft/part04/ValidateISBN.java
+++ src/main/java/com/muditasoft/part04/ValidateISBN.java
@@ -9,11 +9,14 @@ public class ValidateISBN {
 
         int total = 0;
 
-        for (int i = 0; i < 10; i++){
-            if (!Character.isDigit(isbn.charAt(i)))
-                throw new NumberFormatException("ISBN numbers can only contain numeric digits");
-
-            total += isbn.charAt(i) * (10 - i);
+        for (int i = 0; i < 10; i++) {
+            if (!Character.isDigit(isbn.charAt(i))) {
+                if (i == 9 && isbn.charAt(i) == 'X')
+                    total += 10;    // X is 10
+                else
+                    throw new NumberFormatException("ISBN numbers can only contain numeric digits");
+            } else
+                total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
         }
 
         return total % 11 == 0;
