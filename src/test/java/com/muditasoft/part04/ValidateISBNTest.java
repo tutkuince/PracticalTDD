4fed50dfdd9173c582301f9b4a6cf6cf2691bb32
tutkuince <tutku.ince@outlook.com>
2019-03-06 15:45:23 +0300

    checkISBN with another valid isbn number

--- src/test/java/com/muditasoft/part04/ValidateISBNTest.java
+++ src/test/java/com/muditasoft/part04/ValidateISBNTest.java
@@ -5,13 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ValidateISBNTest {
+class ValidateISBNTest {
 
     @Test
     void checkAValidISBN() {
         ValidateISBN validateISBN = new ValidateISBN();
         boolean result = validateISBN.checkISBN("0140449116");
-        assertTrue(result);
+        assertTrue(result, "First Value");
+        result = validateISBN.checkISBN("01401773961");
+        assertTrue(result, "Second Value");
     }
 
     @Test
