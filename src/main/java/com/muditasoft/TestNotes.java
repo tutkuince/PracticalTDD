05b9dc5e0155d54edabdf096aa6adc3edd4bb1a8
tutkuince <tutku.ince@outlook.com>
2019-03-06 18:11:29 +0300

    TestNotes

---
+++ src/main/java/com/muditasoft/TestNotes.java
@@ -0,0 +1,24 @@
+package com.muditasoft;
+
+public class TestNotes {
+    /**
+     *      What is a 'good' or 'bad' test?
+     * Red => Green => Refactor
+     * 1- A new test will initially fail
+     * 2- We work to get the test to pass
+     * 3- We finally optimise our code, and tests
+     *
+     *      Rules of testing
+     * - Test one item of functionality only
+     * - Test business logic, not methods
+     * - Tests must be repeatable, and consistent (LocalDate.now -> LocalDate.of(2019, 1, 1))
+     * - Tests must be thorough
+     *
+     *      What tests should i write?
+     * - What should the logic be?
+     * - What is the opposite to that logic?
+     * - Are there any edge cases?
+     * - Are there any error conditions?
+     *
+     * */
+}
