7df49a943f7715f190c48e97e9b657dc0412f4a2
tutkuince <tutku.ince@outlook.com>
2019-03-06 16:01:39 +0300

    check isbn numbers length not 10

--- src/main/java/com/muditasoft/part04/ValidateISBN.java
+++ src/main/java/com/muditasoft/part04/ValidateISBN.java
@@ -3,6 +3,10 @@ package com.muditasoft.part04;
 public class ValidateISBN {
 
     public boolean checkISBN(String isbn) {
+
+        if (isbn.length() != 10)
+            throw new NumberFormatException("ISBN numbers must be 10 digits long");
+
         int total = 0;
 
         for (int i = 0; i < 10; i++){
