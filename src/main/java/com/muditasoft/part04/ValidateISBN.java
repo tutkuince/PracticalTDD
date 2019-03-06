f807e626d798246d94545c0ce9a6f32216ab188c
tutkuince <tutku.ince@outlook.com>
2019-03-06 17:16:10 +0300

    refactoring for 13 digit isbn -> isThisAValid13DigitISBN 10 digit isbn -> isThisAValid10DigitISBN

--- src/main/java/com/muditasoft/part04/ValidateISBN.java
+++ src/main/java/com/muditasoft/part04/ValidateISBN.java
@@ -5,34 +5,42 @@ public class ValidateISBN {
     public boolean checkISBN(String isbn) {
 
         if (isbn.length() == 13) {
-            int total = 0;
-
-            for (int i = 0; i < 13; i++) {
-                if (i % 2 == 0) {
-                    total += Character.getNumericValue(isbn.charAt(i));
-                } else {
-                    total += Character.getNumericValue(isbn.charAt(i)) * 3;
-                }
-            }
-            return total % 10 == 0;
+            return isThisAValid13DigitISBN(isbn);
         } else {
             if (isbn.length() != 10)
                 throw new NumberFormatException("ISBN numbers must be 10 digits long");
 
-            int total = 0;
-
-            for (int i = 0; i < 10; i++) {
-                if (!Character.isDigit(isbn.charAt(i))) {
-                    if (i == 9 && isbn.charAt(i) == 'X')
-                        total += 10;    // X is 10
-                    else
-                        throw new NumberFormatException("ISBN numbers can only contain numeric digits");
-                } else
-                    total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
-            }
-
-            return total % 11 == 0;
+            return isThisAValid10DigitISBN(isbn);
         }
 
+    }
+
+    private boolean isThisAValid10DigitISBN(String isbn) {
+        int total = 0;
+
+        for (int i = 0; i < 10; i++) {
+            if (!Character.isDigit(isbn.charAt(i))) {
+                if (i == 9 && isbn.charAt(i) == 'X')
+                    total += 10;    // X is 10
+                else
+                    throw new NumberFormatException("ISBN numbers can only contain numeric digits");
+            } else
+                total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
+        }
+
+        return total % 11 == 0;
+    }
+
+    private boolean isThisAValid13DigitISBN(String isbn) {
+        int total = 0;
+
+        for (int i = 0; i < 13; i++) {
+            if (i % 2 == 0) {
+                total += Character.getNumericValue(isbn.charAt(i));
+            } else {
+                total += Character.getNumericValue(isbn.charAt(i)) * 3;
+            }
+        }
+        return total % 10 == 0;
     }
 }
