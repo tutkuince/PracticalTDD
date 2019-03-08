d695abbfd2e7552737de1cf62f9765e98172bde9
tutkuince <tutku.ince@outlook.com>
2019-03-08 15:00:10 +0300

    AssertJ with Collections

--- src/test/java/com/muditasoft/part05/StudentTestWithAssertJAssertions.java
+++ src/test/java/com/muditasoft/part05/StudentTestWithAssertJAssertions.java
@@ -1,5 +1,6 @@
 package com.muditasoft.part05;
 
+import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -29,5 +30,39 @@ public class StudentTestWithAssertJAssertions {
                 .hasSize(5)
                 .matches("^T\\w{3}u$");
 
+    }
+
+    @Test
+    void addCourseToStudent() {
+        final Student stdTutku = new Student("id1", "Tutku", "Ince");
+        final Student stdEmin = new Student("id2", "Emin", "Koklu");
+        final Student stdUgur = new Student("id3", "Ugur", "Batikan");
+        final Student stdAlper = new Student("id4", "Alper", "Omeroglu");
+        final Student stdAral = new Student("id5", "Aral", "Ozbilgi");
+
+        final List<Student> students = List.of(stdTutku, stdEmin, stdUgur, stdAlper, stdAral);
+
+        assertThat(students).as("Student's List")
+                .isNotNull()
+                .isNotEmpty()
+                .hasSize(5)
+                .contains(stdTutku, stdEmin)
+                .containsOnly(stdEmin, stdTutku, stdUgur, stdAlper, stdAral)   // full list must be in containsOnly, order not important
+                .containsExactly(stdTutku, stdEmin, stdUgur, stdAlper, stdAral) // order is important
+                .containsExactlyInAnyOrder(stdEmin, stdTutku, stdUgur, stdAlper, stdAral);  // same as containsOnly
+
+        assertThat(students).as("Student's List with Student's name starts with 'A'")
+                .filteredOn(student -> student.getName().startsWith("A")).hasSize(2)
+                .containsOnly(stdAlper, stdAral);
+
+        assertThat(students).as("Student's List with Student's name starts with 'A'")
+                .filteredOn(new Condition<>() {
+                    @Override
+                    public boolean matches(Student value) {
+                        return value.getName().startsWith("A");
+                    }
+                })
+                .hasSize(2)
+                .containsOnly(stdAlper, stdAral);
     }
 }
